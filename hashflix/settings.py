e3df35f3e1fff96a238bb1914bc019a147cd86f8
Pedro Bittencourt <pedrovbittencourt@gmail.com>
2023-11-04 13:14:35 -0300

    Degub = False

--- hashflix/settings.py
+++ hashflix/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*4ib42akdl%y0mr_0d$xu7x#+b&0e(hp&ncirydaljh649t7t_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
