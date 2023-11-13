87a6f995416afe30c26b16bb21614715aa84cf9b
Pedro Bittencourt <pedrovbittencourt@gmail.com>
2023-11-13 12:20:52 -0300

    Ajeitando a logica da criação automática do admin

--- filme/apps.py
+++ filme/apps.py
@@ -1,19 +1,21 @@
 from django.apps import AppConfig
-
+import os
 
 class FilmeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'filme'
 
-    def ready(self):
-        from .models import Usuario
-        import os
 
-        email = os.getenv("EMAIL_ADMIN")
-        senha = os.getenv("SENHA_ADMIN")
+    if os.getenv("DATABASE_URL"):
+        def ready(self):
+            from .models import Usuario
+            import os
 
-        usuarios = Usuario.objects.filter(email=email)
+            email = os.getenv("EMAIL_ADMIN")
+            senha = os.getenv("SENHA_ADMIN")
 
-        if not usuarios:
-            Usuario.objects.create_superuser(username="admin", email=email, password=senha,
-                                             is_active=True, is_staff=True)
+            usuarios = Usuario.objects.filter(email=email)
+
+            if not usuarios:
+                Usuario.objects.create_superuser(username="a", email=email, password=senha,
+                                                 is_active=True, is_staff=True)
