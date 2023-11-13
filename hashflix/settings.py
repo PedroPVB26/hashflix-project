3d9834e2f0045b09964fc0a11ab48b0a75814128
Pedro Bittencourt <pedrovbittencourt@gmail.com>
2023-11-13 10:21:43 -0300

    Configurando o TOKEN_CSRF, CSRF_TRUSTED_ORIGINS e ALLOWED_HOSTS

--- hashflix/settings.py
+++ hashflix/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*4ib42akdl%y0mr_0d$xu7x#+b&0e(hp&ncirydaljh649t7t_'
+TOKEN_CSRF = os.getenv("TOKEN_CSRF")
+if TOKEN_CSRF:
+    SECRET_KEY = TOKEN_CSRF
+    CSRF_TRUSTED_ORIGINS = ['https://hashflix22.onrender.com']
+else:
+    SECRET_KEY = 'django-insecure-*4ib42akdl%y0mr_0d$xu7x#+b&0e(hp&ncirydaljh649t7t_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["hashflix22.onrender.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
@@ -87,7 +93,6 @@ WSGI_APPLICATION = 'hashflix.wsgi.application'
 # }
 
 import dj_database_url
-import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 if DATABASE_URL:
